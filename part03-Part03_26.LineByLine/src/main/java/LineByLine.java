7931a6f3232413494ab6ff655630415b5e4a2473
Nikhil <nikhildixit2125@gmail.com>
2022-10-15 03:32:59 +0530

    Line by line

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,20 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+//        String a=scanner.nextLine();
+//        String[] pieces = a.split(" ");
+        
+        while(true){
+            String a=scanner.nextLine();
+            if(a.equals("")){
+                break;
+            }
+        String[] pieces = a.split(" ");
+        
+        for(int i=0;i<pieces.length;i++){
+            System.out.println(pieces[i]);
+            }
+        }
     }
 }
