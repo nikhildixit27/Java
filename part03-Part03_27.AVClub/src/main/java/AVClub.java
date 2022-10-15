4015a6947febae297a5680def83a3d98542a25b7
Nikhil <nikhildixit2125@gmail.com>
2022-10-16 05:08:18 +0530

    av club problem

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,22 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            String a =scanner.nextLine();
+            if(a.equals("")){
+                break;
+            }
+        String[] pieces=a.split(" ");
 
+        
+        for(int i=0;i<pieces.length;i++){
+            if(pieces[i].contains("av")){
+                System.out.println(pieces[i]);
+                }
+            }
+        }
+        
 
     }
 }
