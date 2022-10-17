f743969f19228961ba733716b1ff315f4ea06c5f
Nikhil <nikhildixit2125@gmail.com>
2022-10-17 19:26:39 +0530

    first and last word

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,15 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        
+        while(true){
+            String a =scanner.nextLine();
+            if(a.equals("")){
+                break;
+            }
+        String[] pieces=a.split(" ");
+        
+        System.out.println(pieces[0]);
+        }
     }
 }
--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,17 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            String a =scanner.nextLine();
+            if(a.equals("")){
+                break;
+            }
+        String[] pieces=a.split(" ");
+        
+        System.out.println(pieces[pieces.length-1]);        
 
 
+        }
     }
 }
