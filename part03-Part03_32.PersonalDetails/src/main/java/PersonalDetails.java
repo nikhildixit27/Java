6fa368e63bc77f59bdd475b9630f2acdd6a6634b
Nikhil <nikhildixit2125@gmail.com>
2022-10-18 00:15:07 +0530

    personal Details

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,28 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String name="";
+        double sum=0;
+        double count=0;
+        int temp=0;
+        int namelength=0;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String parts[]=input.split(",");
+            sum=sum+ Integer.valueOf(parts[1]);
+            count=count+1;
+            namelength=parts[0].length();
+            
+            if(temp<namelength){
+                temp=namelength;
+                name=parts[0];
+            }
+        }
+        System.out.println("Longest name: "+name);
+        System.out.println("Average of the birth years: "+sum/count);
+        }
     }
-}
+
