148fec60e0a7c0e775b54c9760721c7a26beb32e
Nikhil <nikhildixit2125@gmail.com>
2022-10-17 23:10:44 +0530

    Name age of oldest

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,23 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int age=0;
+        int n=0;
+        String name = "";
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
 
+            String[] col = input.split(",");
+            age=Integer.valueOf(col[1]);
+            if(age>n){
+                n=age;
+                name=col[0];
+                
+            }
+        }
+        System.out.println("Name of the oldest: "+name);
     }
 }
