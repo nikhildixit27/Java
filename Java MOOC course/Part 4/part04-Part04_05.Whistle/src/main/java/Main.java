1eee5b59e4430d2f997f92c41ddc2f2d11d96011
Nikhil <nikhildixit2125@gmail.com>
2022-10-22 00:46:54 +0530

    part4 5. create, initialize, print using class

---
+++ Java MOOC course/Part 4/part04-Part04_05.Whistle/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // This is just an empty main method you can use to test
+        // the Whistle class. Try for example:
+
+        Whistle cowWhistle = new Whistle("Moo");
+        Whistle roosterWhistle = new Whistle("Peef");
+
+        cowWhistle.sound();
+        roosterWhistle.sound();
+        cowWhistle.sound();
+    }
+}
