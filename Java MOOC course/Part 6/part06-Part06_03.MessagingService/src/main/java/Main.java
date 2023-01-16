304be62d5958aac12beef399084a6f8b5f2e22fb
Nikhil <nikhildixit2125@gmail.com>
2023-01-17 00:11:42 +0530

    part6 3. used list object as parameter to object

---
+++ Java MOOC course/Part 6/part06-Part06_03.MessagingService/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        MessagingService ms = new MessagingService(); 
+        
+        System.out.println(ms.getMessages());
+        
+        // Try out your class here
+    }
+}
